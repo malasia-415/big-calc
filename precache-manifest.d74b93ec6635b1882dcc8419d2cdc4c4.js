291dfb3eb294814ad6cf738fbd1a115f359113fb
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-02 05:44:19 -0700

    Updates

---
+++ precache-manifest.d74b93ec6635b1882dcc8419d2cdc4c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e35f87a8d69a41e0aa45e833f2d08a",
+    "url": "/.github.io/react-gh-pages/index.html"
+  },
+  {
+    "revision": "d0c8ef56b44e6d647e2d",
+    "url": "/.github.io/react-gh-pages/static/css/main.064ef5dc.chunk.css"
+  },
+  {
+    "revision": "08d5a13aaaac815d6581",
+    "url": "/.github.io/react-gh-pages/static/js/2.565bb60d.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/.github.io/react-gh-pages/static/js/2.565bb60d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c8ef56b44e6d647e2d",
+    "url": "/.github.io/react-gh-pages/static/js/main.59152357.chunk.js"
+  },
+  {
+    "revision": "a75e59736635710f705c",
+    "url": "/.github.io/react-gh-pages/static/js/runtime-main.cfdf72dd.js"
+  }
+]);
